c103953333974b2714767bf0a946f56b65b4dc9e
marialaravasconcelosqueiros-droid <marialaravasconcelosqueiros@gmail.com>
2026-04-27 11:25:57 -0300

    Add files via upload

---
+++ projeto-programacao/Program2.cs
@@ -0,0 +1,40 @@
+﻿using primeiroProjeto;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sla
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture ) ;
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture ) ;
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture ) ;
+
+            double areaX = x.Area();//Fução matematica raiz > math.sqrt
+
+            double areaY = y.Area();//Função matematica raiz > math.sqrt
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
